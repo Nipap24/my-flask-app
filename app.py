278903af9642e59c3e17e3c416792d1d8cd4a45e
Nipap24 <142407298+Nipap24@users.noreply.github.com>
2025-08-21 14:27:43 +0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
-    return "Hello from Flask on Render!"
+    return render_template_string("""
+      <h1>Mini Flask App</h1>
+      <p>ลองคำนวณ BMI ด้านล่างนี้</p>
+      <form method="post" action="/bmi">
+        <input name="w" placeholder="น้ำหนัก (กก.)" required>
+        <input name="h" placeholder="ส่วนสูง (ซม.)" required>
+        <button type="submit">คำนวณ</button>
+      </form>
+    """)
+
+@app.post("/bmi")
+def bmi():
+    try:
+        w = float(request.form["w"])
+        h = float(request.form["h"]) / 100.0
+        bmi = w / (h*h)
+        return render_template_string("<h2>BMI = {{bmi:.2f}}</h2><a href='/'>กลับ</a>", bmi=bmi)
+    except Exception as e:
+        return f"Error: {e}", 400
